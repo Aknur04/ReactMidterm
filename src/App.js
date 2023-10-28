d7bc19db6757fb9266496fb9697ce4361aa32df8
Aknur Azamatkyzy <ak_azamatkyzy@kbtu.kz>
2023-10-28 15:36:12 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from '../Home';
+import Post from '../Post';
+import Profile from '../Profile';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/post/:postId" component={Post} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <div>
+      <h1>Post Page</h1>
+      {/*jsjgfdhh */}
+    </div>
+  );
+};
+
+export default Post;
